d7702220290da494e44b41c4ec0fc40da7934413
Mr. Vape <alexnlarsen@gmail.com>
2018-06-08 18:01:27 +0200

    more cleanup

--- src/Buttons/MuteButton.py
+++ src/Buttons/MuteButton.py
@@ -11,17 +11,11 @@ class MuteButton:
                                   command=self.mute_sound,
                                   width=3,
                                   borderwidth=0,
-                                  #bd=2,
-                                  padx=0,
-                                  highlightthickness=0,
-                                  highlightcolor="#37d3ff",
-                                  highlightbackground="#37d3ff")
+                                  highlightthickness=0)
 
         self.mute_sound()
 
     def mute_sound(self):
         color = '#7AAD12' if self.et.playing else '#DD0000'
-        # color = '#DDDDDD'
-        s = 36
-        self.mute_button.config(bg=color)  # , image=img, width=s, height=s)#, bd=3)
+        self.mute_button.config(bg=color)
         self.et.playing = not self.et.playing
